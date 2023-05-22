701b7d044b325f18e73e8b51e358a64e5539a48f
Sidney Andrews <sidandrews@microsoft.com>
2023-05-21 21:59:14 -0400

    Remove service code comments

--- Services/CosmosDbService.cs
+++ Services/CosmosDbService.cs
@@ -2,95 +2,49 @@
 
 namespace Cosmos.Chat.GPT.Services;
 
-/// <summary>
-/// Service to access Azure Cosmos DB for NoSQL.
-/// </summary>
 public class CosmosDbService
 {
-    /// <summary>
-    /// Creates a new instance of the service.
-    /// </summary>
-    /// <param name="endpoint">Endpoint URI.</param>
-    /// <param name="key">Account key.</param>
-    /// <param name="databaseName">Name of the database to access.</param>
-    /// <param name="containerName">Name of the container to access.</param>
-    /// <exception cref="ArgumentNullException">Thrown when endpoint, key, databaseName, or containerName is either null or empty.</exception>
-    /// <remarks>
-    /// This constructor will validate credentials and create a service client instance.
-    /// </remarks>
     public CosmosDbService(string endpoint, string key, string databaseName, string containerName)
     {
         ArgumentNullException.ThrowIfNullOrEmpty(databaseName);
         ArgumentNullException.ThrowIfNullOrEmpty(containerName);
     }
 
-    /// <summary>
-    /// Gets a list of all current chat sessions.
-    /// </summary>
-    /// <returns>List of distinct chat session items.</returns>
     public async Task<List<Session>> GetSessionsAsync()
     {
         await Task.Delay(millisecondsDelay: 500);
         return Enumerable.Empty<Session>().ToList();
     }
 
-    /// <summary>
-    /// Gets a list of all current chat messages for a specified session identifier.
-    /// </summary>
-    /// <param name="sessionId">Chat session identifier used to filter messsages.</param>
-    /// <returns>List of chat message items for the specified session.</returns>
     public async Task<List<Message>> GetSessionMessagesAsync(string sessionId)
     {
         await Task.Delay(millisecondsDelay: 500);
         return Enumerable.Empty<Message>().ToList();
     }
 
-    /// <summary>
-    /// Creates a new chat session.
-    /// </summary>
-    /// <param name="session">Chat session item to create.</param>
-    /// <returns>Newly created chat session item.</returns>
     public async Task<Session> InsertSessionAsync(Session session)
     {
         await Task.Delay(millisecondsDelay: 500);
         return session;
     }
 
-    /// <summary>
-    /// Creates a new chat message.
-    /// </summary>
-    /// <param name="message">Chat message item to create.</param>
-    /// <returns>Newly created chat message item.</returns>
     public async Task<Message> InsertMessageAsync(Message message)
     {
         await Task.Delay(millisecondsDelay: 500);
         return message;
     }
 
-    /// <summary>
-    /// Updates an existing chat session.
-    /// </summary>
-    /// <param name="session">Chat session item to update.</param>
-    /// <returns>Revised created chat session item.</returns>
     public async Task<Session> UpdateSessionAsync(Session session)
     {
         await Task.Delay(millisecondsDelay: 500);
         return session;
     }
 
-    /// <summary>
-    /// Batch create or update chat messages and session.
-    /// </summary>
-    /// <param name="messages">Chat message and session items to create or replace.</param>
     public async Task UpsertSessionBatchAsync(params dynamic[] messages)
     {
         await Task.Delay(millisecondsDelay: 500);
     }
 
-    /// <summary>
-    /// Batch deletes an existing chat session and all related messages.
-    /// </summary>
-    /// <param name="sessionId">Chat session identifier used to flag messages and sessions for deletion.</param>
     public async Task DeleteSessionAndMessagesAsync(string sessionId)
     {
         await Task.Delay(millisecondsDelay: 500);
--- Services/OpenAiService.cs
+++ Services/OpenAiService.cs
@@ -2,46 +2,21 @@
 
 namespace Cosmos.Chat.GPT.Services;
 
-/// <summary>
-/// Service to access Azure OpenAI.
-/// </summary>
 public class OpenAiService
 {
     private readonly string _modelName = String.Empty;
 
-    /// <summary>
-    /// Creates a new instance of the service.
-    /// </summary>
-    /// <param name="endpoint">Endpoint URI.</param>
-    /// <param name="key">Account key.</param>
-    /// <param name="modelName">Name of the deployed Azure OpenAI model.</param>
-    /// <exception cref="ArgumentNullException">Thrown when endpoint, key, or modelName is either null or empty.</exception>
-    /// <remarks>
-    /// This constructor will validate credentials and create a HTTP client instance.
-    /// </remarks>
     public OpenAiService(string endpoint, string key, string modelName)
     {
         ArgumentNullException.ThrowIfNullOrEmpty(modelName);
     }
 
-    /// <summary>
-    /// Sends a prompt to the deployed OpenAI LLM model and returns the response.
-    /// </summary>
-    /// <param name="sessionId">Chat session identifier for the current conversation.</param>
-    /// <param name="prompt">Prompt message to send to the deployment.</param>
-    /// <returns>Response from the OpenAI model along with tokens for the prompt and response.</returns>
     public async Task<(string response, int promptTokens, int responseTokens)> GetChatCompletionAsync(string sessionId, string userPrompt)
     {
         await Task.Delay(millisecondsDelay: 500);
         return ("&ltRESPONSE&gt;", 0, 0);
     }
 
-    /// <summary>
-    /// Sends the existing conversation to the OpenAI model and returns a two word summary.
-    /// </summary>
-    /// <param name="sessionId">Chat session identifier for the current conversation.</param>
-    /// <param name="conversation">Prompt conversation to send to the deployment.</param>
-    /// <returns>Summarization response from the OpenAI model deployment.</returns>
     public async Task<string> SummarizeAsync(string sessionId, string userPrompt)
     {
         await Task.Delay(millisecondsDelay: 500);
